5f2c75b9517949794ceb2962b0709a40985bdbd0
japaveh <info@japaveh.nl>
2013-11-15 13:51:56 +0100

    Change the authentication

--- config/module.config.authorize.php
+++ config/module.config.authorize.php
@@ -7,10 +7,6 @@
  * @author      Johan van der Heide <info@japaveh.nl>
  * @copyright   Copyright (c) 2004-2013 Japaveh Webdesign (http://japaveh.nl)
  */
-$program = new \Program\Entity\Program();
-$call    = new \Program\Entity\Call();
-
-
 return array(
     'bjyauthorize' => array(
         // resource providers provide a list of resources that will be tracked
@@ -46,10 +42,7 @@ return array(
             /* If this guard is specified here (i.e. it is enabled), it will block
              * access to all routes unless they are specified here.
              */
-            'BjyAuthorize\Guard\Route' => array(
-                array('route' => 'route-' . $program->get('underscore_full_entity_name'), 'roles' => array()),
-                array('route' => 'route-' . $call->get('underscore_full_entity_name'), 'roles' => array()),
-            ),
+            'BjyAuthorize\Guard\Route' => array(),
         ),
     ),
 );
