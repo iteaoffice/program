f9b20ce5a1399df926a833b2cbd393b98f044d70
Johan van der Heide <info@japaveh.nl>
2017-08-18 11:30:05 +0200

    Small fix for the htmlentities

--- src/View/Helper/LinkAbstract.php
+++ src/View/Helper/LinkAbstract.php
@@ -153,7 +153,7 @@ abstract class LinkAbstract extends AbstractViewHelper
         return sprintf(
             $uri,
             $serverUrl() . $url($this->router, $this->routerParams),
-            htmlentities($this->text),
+            htmlentities((string) $this->text),
             implode(' ', $this->classes),
             in_array($this->getShow(), ['icon', 'button', 'alternativeShow']) ? implode('', $this->linkContent)
                 : htmlentities(implode('', $this->linkContent))
