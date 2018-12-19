8f9f52dbd3b2dfed568c033b528b834b2de6693a
Johan van der Heide <info@japaveh.nl>
2018-12-19 09:46:56 +0100

    Added new files

---
+++ src/ValueObject/ProgramData.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Program\ValueObject;
+
+final class ProgramData
+{
+    private $calls;
+    private $projects;
+    private $partners;
+    private $countries;
+    private $years = 12;
+
+    public function __construct(int $calls, int $projects, int $partners, int $countries, int $years)
+    {
+        $this->calls = $calls;
+        $this->projects = $projects;
+        $this->partners = $partners;
+        $this->countries = $countries;
+        $this->years = $years;
+    }
+
+    public function getCalls(): int
+    {
+        return $this->calls;
+    }
+
+    public function getProjects(): int
+    {
+        return $this->projects;
+    }
+
+    public function getPartners(): int
+    {
+        return $this->partners;
+    }
+
+    public function getCountries(): int
+    {
+        return $this->countries;
+    }
+
+    public function getYears(): int
+    {
+        return $this->years;
+    }
+}
---
+++ src/View/Handler/ProgramHandler.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * ITEA Office all rights reserved
+ *
+ * @category   Program
+ *
+ * @author     Johan van der Heide <johan.van.der.heide@itea3.org>
+ * @copyright  Copyright (c) 2004-2017 ITEA Office (https://itea3.org)
+ * @license    https://itea3.org/license.txt proprietary
+ *
+ * @link       https://itea3.org
+ */
+
+declare(strict_types=1);
+
+namespace Program\View\Handler;
+
+use Program\Service\ProgramService;
+use Zend\Authentication\AuthenticationService;
+use Zend\I18n\Translator\TranslatorInterface;
+use Zend\Mvc\Application;
+use Zend\View\HelperPluginManager;
+use ZfcTwig\View\TwigRenderer;
+
+/**
+ * Class ProgramHandler
+ *
+ * @package Program\View\Helper
+ */
+final class ProgramHandler extends AbstractHandler
+{
+    /**
+     * @var ProgramService
+     */
+    private $programService;
+
+    public function __construct(
+        Application $application,
+        HelperPluginManager $helperPluginManager,
+        TwigRenderer $renderer,
+        AuthenticationService $authenticationService,
+        TranslatorInterface $translator,
+        ProgramService $programService
+    ) {
+        parent::__construct(
+            $application,
+            $helperPluginManager,
+            $renderer,
+            $authenticationService,
+            $translator
+        );
+        $this->programService = $programService;
+    }
+
+    public function __invoke(): self
+    {
+        return $this;
+    }
+
+    public function parseProgramBanner(): string
+    {
+        return $this->renderer->render(
+            'cms/program/banner-frontpage',
+            ['programData' => $this->programService->findProgramData()]
+        );
+    }
+}
